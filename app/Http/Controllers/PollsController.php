0397022bf869b9f5ab52db4992111f0caf2f90b4
Rohan Bajracharya <rbajra18@gmail.com>
2020-10-06 13:27:52 +0545

    resource added for transforming data

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Poll;
+use App\Http\Resources\Poll as PollResource;
 use Illuminate\Http\Request;
 use Validator;
 use Illuminate\Support\Facades\Redis;
@@ -19,7 +20,8 @@ class PollsController extends Controller
         if (is_null($poll)) {
             return response()->json(null, 404);
         }
-        return response()->json(Poll::findOrFail($id), 200);
+        $response = new PollResource(Poll::findOrFail($id), 200);
+        return response()->json($response, 200);
     }
     public function store(Request $request)
     {
