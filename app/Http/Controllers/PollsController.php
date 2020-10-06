b6af056d4180a9c2d56338e9740f2346bcd68cae
Rohan Bajracharya <rbajra18@gmail.com>
2020-10-06 12:15:21 +0545

    method and routes created for adding a poll

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -15,4 +15,8 @@ class PollsController extends Controller
     {
         return response()->json(Poll::find($id), 200);
     }
+    public function store(Request $request) {
+        $poll = Poll::create($request->all());
+        return response() -> json($poll, 201);
+    }
 }
