37a20b9d9e98b537041d0ccb44ee1e12e8112c4d
Rohan Bajracharya <rbajra18@gmail.com>
2020-10-06 12:46:50 +0545

    validation added for adding a poll

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Poll;
 use Illuminate\Http\Request;
+use Validator;
 use Illuminate\Support\Facades\Redis;
 
 class PollsController extends Controller
@@ -22,6 +23,13 @@ class PollsController extends Controller
     }
     public function store(Request $request)
     {
+        $rules = [
+            'title' => 'required|max:255',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
         $poll = Poll::create($request->all());
         return response()->json($poll, 201);
     }
